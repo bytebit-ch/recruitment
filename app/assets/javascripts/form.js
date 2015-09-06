d82827dc1d37fa57b4fa8c0ec654ad9273815eb0
C.Harris <charris@bytebit.ch>
2015-09-06 16:34:45 +0100

    added simple form

---
+++ app/assets/javascripts/form.js
@@ -0,0 +1,24 @@
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+ 
+function prepareInputsForHints() {
+  var inputs = document.getElementsByTagName("input");
+  for (var i=0; i<inputs.length; i++){
+    inputs[i].onfocus = function () {
+      this.parentNode.getElementsByTagName("span")[0].style.display = "inline";
+    }
+    inputs[i].onblur = function () {
+      this.parentNode.getElementsByTagName("span")[0].style.display = "none";
+    }
+  }
+}
+addLoadEvent(prepareInputsForHints);
