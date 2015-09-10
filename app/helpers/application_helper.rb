8b5842b93fb6be288024c72fed5816dece2ecf4d
C.Harris <charris@bytebit.ch>
2015-09-10 14:02:06 +0100

    added gravatar support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def avatar_url(student_profile)
+    	default_url = "#{root_url}images/guest.png"
+    	gravatar_id = Digest::MD5.hexdigest(student_profile.profile_img.downcase)
+    	"http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+  		
+	end
 end
