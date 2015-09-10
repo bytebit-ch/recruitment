884261c1a8e414b489c9b7a9c00415e6af7d9e13
C.Harris <charris@bytebit.ch>
2015-09-10 14:40:24 +0100

    fixed gravatar overriding error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,7 @@
 module ApplicationHelper
+	#gravatar logic
 	def avatar_url(student_profile)
-    	default_url = "#{root_url}images/guest.png"
     	gravatar_id = Digest::MD5.hexdigest(student_profile.profile_img.downcase)
-    	"http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
-  		
+		"http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
 	end
 end
