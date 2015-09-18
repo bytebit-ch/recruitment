aeabb0f3dec6a5b73f4d7a5f7cd2648557b81e49
C.Harris <charris@bytebit.ch>
2015-09-18 10:56:04 +0100

    fixed gravatar blurry looking photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
 	#gravatar logic
 	def avatar_url(student_profile)
     	gravatar_id = Digest::MD5.hexdigest(student_profile.user.email.downcase)
-		"http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+		"http://gravatar.com/avatar/#{gravatar_id}.png?s=150"
 	end
 end
