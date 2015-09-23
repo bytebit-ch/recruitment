4e3e3c8b76d91a04afe98803d13343c959ca3725
C.Harris <charris@bytebit.ch>
2015-09-23 13:06:53 +0100

    put in validation

--- app/models/business_profile.rb
+++ app/models/business_profile.rb
@@ -1,4 +1,6 @@
 class BusinessProfile < ActiveRecord::Base
 	belongs_to :user
 	has_many :business_listings
+
+	validates_presence_of :business_name
 end
--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -1,3 +1,4 @@
 class StudentProfile < ActiveRecord::Base
 	belongs_to :user
+	validates_presence_of :first_name, :last_name
 end
