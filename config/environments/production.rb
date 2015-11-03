5cc2603494beef4870873fcf90cfc4db1b4dfaf2
Tom Luce <thomasgluce@gmail.com>
2015-11-03 16:45:20 +0000

    Few styling tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {
-   host: "cloudcentre.calligocloud.net"
+   host: "djcp.co.uk"
  }
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
-  
+
+  root 'landing#index'
   get 'landing/index' => 'landing#index', as: :landing
 
  # get 'student_profiles' => 'student_profiles#index', as: :student_profiles
- 
+
   #get 'business_profiles' => 'business_profiles#index', as: :business_profiles
   # get 'users/profile'
   get 'admin' => 'admin#index', as: :admin
-
-  root 'landing#index'
 
   resources :business_listings
   resources :business_profiles
   resources :student_profiles
   resources :admin
-  
+
   devise_for :users, :controllers => {
     :invitations => 'users/invitations',
     :registrations => "users/registrations"
@@ -22,5 +21,5 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
 
-  
+
 end
